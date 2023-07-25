982f5370f62de1cba82ca1fd586e653fada3d987
hjuanma <jm1995217@hotmail.com>
2023-07-25 18:22:21 -0500

    register screen added

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,4 @@
-export 'home_screen.dart';
-export 'cubits/cubit_counter_screen.dart';
 export 'blocs/bloc_counter_screen.dart';
+export 'cubits/cubit_counter_screen.dart';
+export 'home_screen.dart';
+export 'user/register_screen.dart';
